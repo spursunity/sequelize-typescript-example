881a23424a615a6f27d751a8b84d56aeaf82b502
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-10-18 12:32:39 +0300

    delete dist

--- dist/imp.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const abc = 'es2015 modules';
-exports.default = { abc };
-//# sourceMappingURL=imp.js.map
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const imp_1 = require("./imp");
-function func() {
-    console.log(imp_1.default.abc);
-}
-func();
-//# sourceMappingURL=index.js.map
